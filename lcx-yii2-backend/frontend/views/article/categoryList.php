2b08592a5406c490716efacddf7a835aced6a3d4
feng <123@qq.com>
2020-08-05 19:07:27 +0800

    udpate

--- lcx-yii2-backend/frontend/views/article/categoryList.php
+++ lcx-yii2-backend/frontend/views/article/categoryList.php
@@ -1,1 +1,18 @@
-<div>asdfasdf</div>
+<div class="section">
+    <div class="content-wrap">
+        <div class="container">
+            <div class="row">
+                <div class="comments-box">
+                <?php foreach ($list as $v): ?>
+                <div class="media comment">
+                    <img class="mr-3" src="<?=$v['cover']?>">
+                    <div class="media-body">
+                        <h2 class="media-heading mt-0 mb-1"><small class="date"><?=$v['title']?></small></h2>
+                    </div>
+                </div>
+                <?php endforeach; ?>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
